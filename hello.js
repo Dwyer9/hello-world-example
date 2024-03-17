6467a5e4cbaaf196a657e83491c63257e8438ef3
Mathew Dwyer <mdwyer139@gmail.com>
2024-03-18 09:57:06 +1100

    Lesson 6

--- hello.js
+++ hello.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 
+const showEnglishView = (req, res) => {
+  res.render("hello-world-english");
+};
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("hello-world-english");
+app.get("/", showEnglishView);
+app.get("/english", showEnglishView);
+
+app.get("/french", (req, res) => {
+  res.render("hello-world-french");
+});
+
+app.get("/serbian", (req, res) => {
+  res.render("hello-world-serbian");
 });
 
 app.listen(3000, "localhost", () => {
